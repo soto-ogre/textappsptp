147c502aec21bded672764677179cae2aecbe61e
soto-ogre <soto1oog@gmail.com>
2020-04-09 23:02:39 +0900

    up

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Textapp
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
     
 
     # Settings in config/environments/* take precedence over those specified here.
